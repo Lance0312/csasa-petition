848149989da9c21ff0309c46eb0d6b20b0b6186a
Lance Chen <cyen0312@gmail.com>
2014-03-29 05:27:23 +0800

    Restructure controller code blocks
    Signed-off-by: Lance Chen <cyen0312@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 var url = "data/mly-8.json";
 
-angular.module('app', ['legislatorFilters', 'stringFilters', 'firebase'])
-    .controller('LegislatorListCtrl', function ($scope, $http, $firebase) {
-        var dataRef = new Firebase('https://csasa-petition.firebaseio.com/');
-        $scope.legislators = $firebase(dataRef);
+var app = angular.module('app', ['legislatorFilters', 'stringFilters', 'firebase']);
 
-        $scope.legislators.$on('loaded', function (data) {
-            renderChart1(data);
-        });
+app.controller('LegislatorListCtrl', function ($scope, $http, $firebase) {
+    var dataRef = new Firebase('https://csasa-petition.firebaseio.com/');
+    $scope.legislators = $firebase(dataRef);
 
-        $scope.choose = function (id, choice) {
-            if (typeof choice != 'undefined') {
-                $scope.legislators.$child(id).$update({'choice': choice});
-            }
-        };
+    $scope.legislators.$on('loaded', function (data) {
+        renderChart1(data);
     });
+
+    $scope.choose = function (id, choice) {
+        if (typeof choice != 'undefined') {
+            $scope.legislators.$child(id).$update({'choice': choice});
+        }
+    };
+});
