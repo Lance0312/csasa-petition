798f122bd33a3b4cfd8a9bd774a0af27cad7c895
Lance Chen <cyen0312@gmail.com>
2014-04-04 19:13:17 +0800

    Rearrange code layout to make line width fit in 79 chars
    Signed-off-by: Lance Chen <cyen0312@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,98 +1,110 @@
-var url = "data/mly-8.json";
+var url = "https://csasa-petition.firebaseio.com/";
 
-var app = angular.module('app', ['legislatorFilters', 'stringFilters', 'firebase']);
+var app = angular.module('app', [
+    'legislatorFilters',
+    'stringFilters',
+    'firebase'
+]);
 
-app.service('authService', ['$firebaseSimpleLogin', function($firebaseSimpleLogin) {
-    var authRef = new Firebase('https://csasa-petition.firebaseio.com/auth');
+app.service('authService', ['$firebaseSimpleLogin',
+    function($firebaseSimpleLogin) {
+        var authRef = new Firebase(url + '/auth');
 
-    return {
-        getHandler: function () {
-            return $firebaseSimpleLogin(authRef);
-        },
-        log: function (data) {
-            return $firebase(authRef).$child('log').$add(data);
-        }
-    };
-}]);
+        return {
+            getHandler: function () {
+                return $firebaseSimpleLogin(authRef);
+            },
+            log: function (data) {
+                return $firebase(authRef).$child('log').$add(data);
+            }
+        };
+    }
+]);
 
 app.service('dataService', ['$firebase', function ($firebase) {
-    var ref = new Firebase('https://csasa-petition.firebaseio.com/data');
+    var ref = new Firebase(url + '/data');
     return $firebase(ref);
 }]);
 
-app.controller('ChartCtrl', ['$scope', '$window', 'dataService', function ($scope, $window, dataService) {
-    dataService.$on('value', function (d) {
-        renderChart1(d.snapshot.value);
-
-        angular.element($window).bind('resize', function () {
+app.controller('ChartCtrl', ['$scope', '$window', 'dataService',
+    function ($scope, $window, dataService) {
+        dataService.$on('value', function (d) {
             renderChart1(d.snapshot.value);
-        });
-    });
-}]);
 
-app.controller('LegislatorListCtrl', ['$scope', 'authService', 'dataService', function ($scope, authService, dataService) {
-    dataService.$bind($scope, 'legislators');
-
-    $scope.auth = authService.getHandler();
-    $scope.choose = function (id, choice) {
-        if (typeof choice != 'undefined') {
-            dataService.$child(id).$update({'choice': choice})
-                .then(function (result) {
-                    authService.log({
-                        user: $scope.auth.user,
-                        legislator: id,
-                        choice: choice,
-                        timestamp: new Date().getTime()
-                    });
-                }, function (error) {
-                    if (error.code == 'PERMISSION_DENIED') {
-                    }
-                });
-        }
-    };
-}]);
-
-app.controller('AuthCtrl', ['$scope', 'authService', function ($scope, authService) {
-    $scope.auth = authService.getHandler();
-    $scope.checkAuth = function () {
-        $scope.auth.$getCurrentUser()
-            .then(function (result) {
-                if (result === null) {
-                    $('#auth-panel').modal('show');
-                }
-            }, function (error) {
+            angular.element($window).bind('resize', function () {
+                renderChart1(d.snapshot.value);
             });
-    };
-    $scope.login = function (provider, options) {
-        $scope.auth.$login(provider, options);
-        $('#auth-panel').modal('hide');
-    };
-    $scope.logout = function () {
-        $scope.auth.$logout();
-    };
-    $scope.getAvatar = function () {
-        if ($scope.auth.user === null) {
-            return null;
-        }
-        switch ($scope.auth.user.provider) {
-            case 'facebook':
+        });
+    }
+]);
+
+app.controller('LegislatorListCtrl', ['$scope', 'authService', 'dataService',
+    function ($scope, authService, dataService) {
+        dataService.$bind($scope, 'legislators');
+
+        $scope.auth = authService.getHandler();
+        $scope.choose = function (id, choice) {
+            if (typeof choice != 'undefined') {
+                dataService.$child(id).$update({'choice': choice})
+                    .then(function (result) {
+                        authService.log({
+                            user: $scope.auth.user,
+                            legislator: id,
+                            choice: choice,
+                            timestamp: new Date().getTime()
+                        });
+                    }, function (error) {
+                        if (error.code == 'PERMISSION_DENIED') {
+                        }
+                    });
+            }
+        };
+    }
+]);
+
+app.controller('AuthCtrl', ['$scope', 'authService',
+    function ($scope, authService) {
+        $scope.auth = authService.getHandler();
+        $scope.checkAuth = function () {
+            $scope.auth.$getCurrentUser()
+                .then(function (result) {
+                    if (result === null) {
+                        $('#auth-panel').modal('show');
+                    }
+                }, function (error) {
+                });
+        };
+        $scope.login = function (provider, options) {
+            $scope.auth.$login(provider, options);
+            $('#auth-panel').modal('hide');
+        };
+        $scope.logout = function () {
+            $scope.auth.$logout();
+        };
+        $scope.getAvatar = function () {
+            if ($scope.auth.user === null) {
                 return null;
-                break;
-            case 'github':
-                return $scope.auth.user.avatar_url;
-                break;
-            case 'google':
-                return $scope.auth.user.thirdPartyUserData.picture;
-                break;
-            case 'twitter':
-                return $scope.auth.user.profile_image_url_https;
-                break;
-            default:
-                return null;
-                break;
-        }
-    };
-}]);
+            }
+            switch ($scope.auth.user.provider) {
+                case 'facebook':
+                    return null;
+                    break;
+                case 'github':
+                    return $scope.auth.user.avatar_url;
+                    break;
+                case 'google':
+                    return $scope.auth.user.thirdPartyUserData.picture;
+                    break;
+                case 'twitter':
+                    return $scope.auth.user.profile_image_url_https;
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        };
+    }
+]);
 
 function checkKeyStrokes (first, second) {
     if (!first || !second) {
@@ -113,6 +125,7 @@ function checkKeyStrokes (first, second) {
 }
 
 var passcodeQueue = [];
+var ma_avatar = 'http://image.kmt.org.tw/people/20090606164842.jpg';
 
 app.directive('ngPasscode', [function () {
     return function (scope, element, attrs) {
@@ -126,7 +139,7 @@ app.directive('ngPasscode', [function () {
             if (checkKeyStrokes(passcodeQueue, [97, 97, 98, 98, 97, 98, 99])) {
                 $('.party-KMT .avatar img')
                     .attr('width', 128)
-                    .attr('src', 'http://image.kmt.org.tw/people/20090606164842.jpg');
+                    .attr('src', ma_avatar);
             }
         });
     };
